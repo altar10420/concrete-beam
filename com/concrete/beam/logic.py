4d6ed59345c28ad1fd45e3dae771e58f77869811
Piotr Śrótwa <altar10420@gmail.com>
2022-02-02 23:47:07 +0100

    add first logic

---
+++ com/concrete/beam/logic.py
@@ -0,0 +1,8 @@
+load = 20
+span = 2
+
+momentum = (load * span * span) / 8
+
+
+def calculate_momentum():
+    return momentum
---
+++ com/concrete/beam/main.py
@@ -0,0 +1,9 @@
+from . import logic
+
+
+def main():
+    print(logic.calculate_momentum())
+
+
+if __name__ == "__main__":
+    main()
